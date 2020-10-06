58f6c853b60595e89278e02e21c5a67d80772a5a
berabjesus <64360974+Berabjesus@users.noreply.github.com>
2020-10-06 21:38:34 +0300

    ADD attendance destroy method to attendance controller

--- app/controllers/attendances_controller.rb
+++ app/controllers/attendances_controller.rb
@@ -5,7 +5,15 @@ class AttendancesController < ApplicationController
     @attend.event_id =  params[:event_id]
     if @attend.valid? && @attend.save
       redirect_to events_path, notice: 'Event added to attending list'
-    else
+    end
+  end
+
+  def destroy
+    @attend = Attendance.find_by(event_id: params[:id])
+    # @attend.user_id = current_user.id
+    # @attend.event_id =  params[:event_id]
+    if @attend.valid? && @attend.destroy
+      redirect_to events_path, notice: 'Event removed from attending list'
     end
   end
 end
